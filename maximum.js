93db438eab5f5b4da08b89d0b3c6181e17385617
Alejandro Leal Lozano <faleall@unincca.edu.co>
2022-07-16 12:36:41 -0500

    Improvement in the Code Challenge

--- maximum.js
+++ maximum.js
@@ -1,19 +1,21 @@
+/**
+ * Function in charge of calculating the largest absolute difference between all the possible ways of dividing the input Array in two parts.
+ * The map() function is used to traverse the input Array and return a new Array with the maximum differences found.
+ * The slice() function is used to make a copy of a part of the input Array without modifying it.
+ * The Math.abs() function is used to calculate the absolute value.
+ * The Math.max() function is used to calculate the maximum value of an Array.
+ * @param {Array} A 
+ * @returns Integer
+ */
 function calculateMaximumAbsoluteDifference(A) {
-  let firstPart = []; // Array of the first part
-  let secondPart = []; // Array of the second part
-  let V = 0; // Final result
-  let diff = 0; // Variable to store difference
-  for (let iter = 0; iter < A.length; iter++) { // Each position of the array A received as a parameter is traversed.
-    // To split the array A in 2, the slice method is used, in order to split into the previously defined arrays according to the index that is being iterated at the moment.
-    firstPart = A.slice(0, iter + 1)
-    secondPart = A.slice(iter + 1)
-    if (secondPart.length > 0) { // Validates that the secondPart array has data
-      diff = Math.abs(Math.max(...firstPart) - Math.max(...secondPart)); // The difference of the maximum value found between the two arrays (firstPart, secondPart) is calculated.
-      // It is validated that the absolute difference is greater than the value of V to reset the value of V
-      if (diff > V) {
-        V = diff;
-      }
-    }
-  }
-  return V; // Return the valu of V
+  let firstPart = [], secondPart = [], diff = 0; // Definition of variables.
+  // The array A is traversed with the MAP function to return a new array with the differences of the maximum values of each part of the array.
+  let response = A.map(function (currentValue, index) {
+    // The slice function is used to create new arrays from array A.
+    firstPart = A.slice(0, index + 1);
+    secondPart = A.slice(index + 1);
+    // The difference of the maximum value found between the two arrays (firstPart, secondPart) is calculated.
+    return (secondPart.length > 0) ? Math.abs(Math.max(...firstPart) - Math.max(...secondPart)) : diff;
+  });
+  return Math.max(...response); // The maximum value of the array returned from the MAP function is returned.
 }
